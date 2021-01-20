e4f54e2bc7e749843cc34b42f5c9b9a510200eb6
James Yuzawa <jtyuzawa@gmail.com>
2021-01-20 14:32:31 -0500

    Update polyhedrasaver.rb

--- Casks/polyhedrasaver.rb
+++ Casks/polyhedrasaver.rb
@@ -1,6 +1,6 @@
 cask 'polyhedrasaver' do
-  version '2.0.0'
-  sha256 '2900cc1af918b7777d026d7fd8cd67570faf94c3c7d69c33bc362c809b56a363'
+  version '2.1.0'
+  sha256 '104a6054963e920114d11aac373db8212b1f49b3d2ad95018b385ab07e6c2cc5'
 
   url "https://github.com/yuzawa-san/PolyhedraSaver/releases/download/v#{version}/Polyhedra.saver.zip"
   appcast 'https://github.com/yuzawa-san/PolyhedraSaver/releases.atom'
