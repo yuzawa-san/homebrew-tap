c670feee0d7dec1ec2d4d90d354dc25f27d33b30
James Yuzawa <jtyuzawa@gmail.com>
2022-12-27 22:21:54 -0500

    try auto updateing

--- Casks/polyhedrasaver.rb
+++ Casks/polyhedrasaver.rb
@@ -3,9 +3,16 @@ cask 'polyhedrasaver' do
   sha256 '932f7ca40e62a8cd191ead7b9e0eb915a1d6f227d44c1e5cbf8fb4a917035b4d'
 
   url "https://github.com/yuzawa-san/PolyhedraSaver/releases/download/v#{version}/Polyhedra.saver.zip"
-  appcast 'https://github.com/yuzawa-san/PolyhedraSaver/releases.atom'
   name 'PolyhedraSaver'
+  desc 'A screensaver with various convex polyhedra bouncing across the screen.'
   homepage 'https://github.com/yuzawa-san/PolyhedraSaver'
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: ">= :catalina"
 
   screen_saver 'Polyhedra.saver'
 end
