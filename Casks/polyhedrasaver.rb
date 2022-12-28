82dd5ae5723676abb4c6f22d3947e57a2ecc286d
James Yuzawa <jtyuzawa@gmail.com>
2022-12-27 22:30:35 -0500

    try autoupdating

--- Casks/polyhedrasaver.rb
+++ Casks/polyhedrasaver.rb
@@ -1,6 +1,6 @@
 cask 'polyhedrasaver' do
-  version '2.1.8'
-  sha256 '932f7ca40e62a8cd191ead7b9e0eb915a1d6f227d44c1e5cbf8fb4a917035b4d'
+  version :latest
+  sha256 :no_check
 
   url "https://github.com/yuzawa-san/PolyhedraSaver/releases/download/v#{version}/Polyhedra.saver.zip"
   name 'PolyhedraSaver'
